b56a9872b400e10fc8331ffce1347956b9e46ae8
Bruno <bruno.fdm93@gmail.com>
2022-11-02 23:05:12 -0700

    adds crud operations to Post

--- src/entities/Post.ts
+++ src/entities/Post.ts
@@ -1,16 +1,18 @@
-import { Entity, PrimaryKey, Property } from '@mikro-orm/core'
+import { Entity, OptionalProps, PrimaryKey, Property } from '@mikro-orm/core'
 import { ObjectType, Field, Int } from 'type-graphql';
 
 @ObjectType()
 @Entity()
 export class Post {
+  [OptionalProps]: "updatedAt" | "createdAt";
+
   @Field(() => Int)
   @PrimaryKey()
   id!: number;
 
   @Field(() => String)
   @Property({ type: 'date' })
-  createdAt = new Date();
+  createdAt: Date = new Date();
 
   @Field(() => String)
   @Property({ type: 'date', onUpdate: () => new Date() })
@@ -18,5 +20,5 @@ export class Post {
 
   @Field(() => String)
   @Property({ type: "text" })
-  title!: string;
+  title: string;
 }
--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -1,6 +1,6 @@
 import { Post } from "../entities/Post";
 import { MyContext } from "src/types";
-import { Resolver, Query, Ctx, Arg, Int } from "type-graphql";
+import { Resolver, Query, Ctx, Arg, Int, Mutation } from "type-graphql";
 
 @Resolver()
 export class PostResolver {
@@ -8,10 +8,52 @@ export class PostResolver {
     posts(@Ctx() { em }: MyContext): Promise<Post[]> {
         return em.find(Post, {})
     }
+
     @Query(() => Post, { nullable: true })
     postById(
         @Arg('id', () => Int) id: number,
         @Ctx() { em }: MyContext): Promise<Post | null> {
         return em.findOne(Post, { id })
+    }
+
+    @Mutation(() => Post)
+    async createPost(
+        @Arg('title') title: string,
+        @Ctx() { em }: MyContext): Promise<Post> {
+        const post = em.create(Post, { title })
+        await em.persistAndFlush(post)
+        return post
+    }
+
+    @Mutation(() => Post, { nullable: true })
+    async updatePost(
+        @Arg('id') id: number,
+        @Arg('title') title: string,
+        @Ctx() { em }: MyContext): Promise<Post | null> {
+
+        const post = await em.findOne(Post, { id })
+        if (!post) {
+            return null
+        }
+        if (typeof title !== 'undefined') {
+            post.title = title
+            em.persistAndFlush(post)
+        }
+        return post;
+    }
+
+    @Mutation(() => Boolean)
+    async deletePost(
+        @Arg('id') id: number,
+        @Ctx() { em }: MyContext): Promise<boolean> {
+        try {
+            await em.findOneOrFail(Post, { id })
+        }
+        catch (err) {
+            console.log(`error: ${err}`)
+            return false
+        }
+        em.nativeDelete(Post, { id })
+        return true
     }
 }
